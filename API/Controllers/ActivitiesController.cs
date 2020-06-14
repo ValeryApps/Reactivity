6dc9aa443ed1b4bf4c0c585ba5b11e6fbb782822
ValeryApps <valeryghn10@outlook.com>
2020-06-13 22:26:01 -0700

    Third commit

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,13 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.Activities;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class ActivitiesController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivitiesController : ControllerBase
     {
-        // GET
-        public IActionResult Index()
+        private readonly IMediator _mediator;
+
+        public ActivitiesController(IMediator mediator)
         {
-            return View();
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> List()
+        {
+            var activities = await _mediator.Send(new List.Query());
+            return activities;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> Details(Guid id)
+        {
+            var activity = await _mediator.Send(new Details.Query {Id = id});
+            return activity;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Create(Create.Command command)
+        {
+            var unit = await _mediator.Send(command);
+            return unit;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+        {
+            command.Id = id;
+            var unit = await _mediator.Send(command);
+            return unit;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Delete(Guid id)
+        {
+            return await _mediator.Send(new Delete.Command {Id = id});
         }
     }
 }
--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,7 +1,52 @@
-﻿namespace Application.Activities
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
 {
     public class Create
     {
+        public class Command:IRequest
+        {
+            public Guid Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string Category { get; set; }
+            public string Venue { get; set; }
+            public string City { get; set; }
+            public DateTime Date { get; set; }
+        }
         
+        public class Handler:IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = new Activity
+                {
+                    Id = request.Id,
+                    Category = request.Category,
+                    City = request.City,
+                    Date = request.Date,
+                    Description = request.Description,
+                    Title = request.Title,
+                    Venue = request.Venue
+                };
+                _context.Activities.Add(activity);
+                if (await _context.SaveChangesAsync() > 0)
+                {
+                    return Unit.Value;
+                }
+                throw new Exception("Failed to save resources to the database");
+            }
+        }
     }
 }
